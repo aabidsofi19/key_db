960aa8b0c560ac796c55d32d6f016e9ce9c8e3b6
aabidsofi19 <mailtoaabid01@gmail.com>
2023-06-03 19:07:58 +0530

    Cleanup Lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
-// #![feature(iterator_try_collect)]
-
 mod aol;
 mod db;
 mod utils;
@@ -8,7 +6,6 @@ use crate::db::{load, Db};
 use pyo3::prelude::*;
 use pyo3::Python;
 
-/// A Python module implemented in Rust.c
 #[pymodule]
 fn layer_db(_py: Python, m: &PyModule) -> PyResult<()> {
     pyo3_log::init();
