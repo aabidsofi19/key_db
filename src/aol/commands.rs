af7d65387b2857b8f1dbd4581c77796b9dbae52b
aabidsofi19 <mailtoaabid01@gmail.com>
2023-06-05 18:18:55 +0530

    Refactor Comments and const names

--- src/aol/commands.rs
+++ src/aol/commands.rs
@@ -2,12 +2,11 @@ use crate::utils::int::read_be_u32;
 use core::ops::Range;
 use serde_json::Value;
 
-const KEY_SIZE_BYTES: usize = 1;
-const VALUE_SIZE_BYTES: usize = 4;
-const COMMAND_SIZE: usize = 1;
-const COMMAND_INDEX: usize = 4;
+const KEY_SIZE_BYTES_LENGTH: usize = 1;
+const VALUE_SIZE_BYTES_LENGTH: usize = 4;
+const COMMAND_BYTE_LENGTH: usize = 1;
+const COMMAND_INDEX_OFFSET: usize = 4;
 const TOTAL_SIZE_INDEX: Range<usize> = 0..4;
-
 
 pub trait Loggable<T> {
     fn action_value() -> u8;
@@ -32,15 +31,22 @@ impl Loggable<RemoveCommand> for RemoveCommand {
     }
 
     fn to_log(&self) -> Vec<u8> {
-        // RemoveCommand Log Structure
-        // [Totol_size u32][command_value u8][key_size u8][key value upto u8 bytes]
+        // RemoveCommand Log Structure:
+        // [Total_size: u32 | Command_value: u8 | Key_size: u8 | Key: ...up to u8 bytes]
+        // [ 1,  0,  0,   1 |                 1 |            3 |             10, 12, 11]
+
+        // Components:
+        // - Total_size: 32-bit unsigned integer representing the total size of the log entry in bytes.
+        // - Command_value: 8-bit unsigned integer representing the command value for RemoveCommand (2).
+        // - Key_size: 8-bit unsigned integer representing the size of the key in bytes.
+        // - Key: The key value itself, up to u8 bytes in length.
 
         let mut log: Vec<u8> = vec![];
 
         let key = self.key.as_bytes();
         let key_size = key.len() as u8;
 
-        let total_size = (COMMAND_SIZE + KEY_SIZE_BYTES + (key_size as usize)) as u32;
+        let total_size = (COMMAND_BYTE_LENGTH + KEY_SIZE_BYTES_LENGTH + (key_size as usize)) as u32;
 
         log.extend_from_slice(&total_size.to_be_bytes());
 
@@ -61,7 +67,7 @@ impl Loggable<RemoveCommand> for RemoveCommand {
             return Err("Corupt Log".to_string());
         };
 
-        if log[COMMAND_INDEX] != RemoveCommand::action_value() {
+        if log[COMMAND_INDEX_OFFSET] != RemoveCommand::action_value() {
             return Err("Log command is not Remove".to_string());
         };
 
@@ -78,21 +84,30 @@ impl Loggable<SetCommand> for SetCommand {
     }
 
     fn to_log(&self) -> Vec<u8> {
-        // Set log structure
-        //  [total size u32][command_value i.e 1][key size
-        //  u8][k][e][y][value][size][i][32][v][a][l][u][e]
 
+        // Set log structure:
+        // [total size: u32 | command_value: u8 | key size: u8 | key      | value size: u32 | value  ]
+        // [     1,  0,  0 1|                 1 |           3  | 10,1,22  |      3          |   1,2,4]
+
+        // Components:
+        // - total size: 32-bit unsigned integer representing the total size of the log entry in bytes.
+        // - command_value: 8-bit unsigned integer representing the command value for SetCommand (1).
+        // - key size: 8-bit unsigned integer representing the size of the key in bytes.
+        // - key: The key value itself.
+        // - value size: 32-bit unsigned integer representing the size of the value in bytes.
+        // - value: The value associated with the key.
+        
+        
         let mut log: Vec<u8> = vec![];
-        // let v: Value = Python::with_gil(|py| depythonize(self.value.as_ref(py)).unwrap());
         let value = serde_json::to_string(&self.value).unwrap().into_bytes();
         let value_length = value.len() as u32;
         let key = self.key.as_bytes();
 
         let key_len = key.len() as u8;
-        let total_size: u32 = (COMMAND_SIZE
-            + KEY_SIZE_BYTES
+        let total_size: u32 = (COMMAND_BYTE_LENGTH
+            + KEY_SIZE_BYTES_LENGTH
             + key_len as usize
-            + VALUE_SIZE_BYTES
+            + VALUE_SIZE_BYTES_LENGTH
             + value_length as usize) as u32;
 
         log.extend_from_slice(&total_size.to_be_bytes());
@@ -120,7 +135,7 @@ impl Loggable<SetCommand> for SetCommand {
             return Err("Corupt Log".to_string());
         };
 
-        if log[COMMAND_INDEX] != SetCommand::action_value() {
+        if log[COMMAND_INDEX_OFFSET] != SetCommand::action_value() {
             return Err("Log command is not Set".to_string());
         };
 
@@ -153,7 +168,7 @@ impl LogCommand {
     }
 
     pub fn from_log_bytes(log: &[u8]) -> Result<LogCommand, String> {
-        match log[COMMAND_INDEX] {
+        match log[COMMAND_INDEX_OFFSET] {
             1 => {
                 let set_command = SetCommand::from_log(log).unwrap();
                 Ok(LogCommand::Set(set_command))
@@ -188,10 +203,10 @@ mod tests {
             value: json!("test".to_string()),
         })
     }
-    
+
     fn remove_command() -> LogCommand {
-        LogCommand::Remove(RemoveCommand{
-            key:"test".to_string()
+        LogCommand::Remove(RemoveCommand {
+            key: "test".to_string(),
         })
     }
 
@@ -215,12 +230,11 @@ mod tests {
         assert_eq!(command.to_log_bytes(), set_command_bytes());
     }
 
-
     #[test]
     fn remove_command_to_bytes() {
-        let bytes = remove_command().to_log_bytes() ;
-        
+        let bytes = remove_command().to_log_bytes();
+
         // assert_eq!(bytes.len(),remove_command_bytes().len()) ;
-        assert_eq!(bytes,remove_command_bytes());
+        assert_eq!(bytes, remove_command_bytes());
     }
 }
